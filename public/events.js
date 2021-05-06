5c2f36d6c2ca95e408473d2ef70be0413d63b53c
Matt Melendez <matthewemelendez@gmail.com>
2021-05-06 13:59:05 -0700

    Halfway through upvote and downvote

--- public/events.js
+++ public/events.js
@@ -8,6 +8,36 @@ const fetchPic = async () => {
     }
 }
 
+const upvote = async () => {
+    const res = await fetch("/kitten/upvote", {
+        method: "PATCH",
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({score : kitten.score})
+    })
+
+    if (res.ok) {
+        return res.score;
+    } else {
+        window.alert("Upvote isn't working! Sorry!")
+    }
+}
+
+
+const downvote = async () => {
+    const res = await fetch("/kitten/downvote", {
+        method: "PATCH",
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({score : kitten.score})
+    })
+
+    if (res.ok) {
+        return res.score;
+    } else {
+        window.alert("Downvote isn't working! Sorry!")
+    }
+}
+
+
 window.addEventListener("DOMContentLoaded", () =>{
 
     let catPic = document.querySelector(".cat-pic")
@@ -15,14 +45,25 @@ window.addEventListener("DOMContentLoaded", () =>{
     let loader = document.querySelector(".loader");
 
     picButton.addEventListener("click", async () => {
-
         loader.innerHTML = "Loading..."
         let currPic = await fetchPic();
         catPic.setAttribute("src", currPic);
         loader.innerHTML = "";
-
     })
 
+    let score = document.querySelector(".score")
+    let upvoteButton = document.getElementById("upvote")
+    let downvoteButton = document.getElementById("downvote")
+
+    upvoteButton.addEventListener("click", async () => {
+        let currValue = await upvote()
+        score.innerHTML = currValue;
+    })
+
+    downvoteButton.addEventListener("click", async () => {
+        let currValue = await downvote()
+        score.innerHTML = currValue;
+    })
 
 
 })

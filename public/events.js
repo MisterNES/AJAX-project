a620d9450c094f7e0b94eb0493d3b22da0429917
Nathan Darrow <nathanadarrow@gmail.com>
2021-05-06 13:29:48 -0700

    Phases 3 Complete

--- public/events.js
+++ public/events.js
@@ -2,7 +2,9 @@ const fetchPic = async () => {
     const res = await fetch("/kitten/image")
     const json = await res.json()
     if (res.ok) {
-        return json.src
+        return json.src;
+    } else {
+        window.alert("Oops! Something went wrong! Please try again.");
     }
 }
 
@@ -10,10 +12,15 @@ window.addEventListener("DOMContentLoaded", () =>{
 
     let catPic = document.querySelector(".cat-pic")
     let picButton = document.getElementById("new-pic")
+    let loader = document.querySelector(".loader");
 
     picButton.addEventListener("click", async () => {
+
+        loader.innerHTML = "Loading..."
         let currPic = await fetchPic();
-        catPic.setAttribute("src", currPic)
+        catPic.setAttribute("src", currPic);
+        loader.innerHTML = "";
+
     })
 
 
